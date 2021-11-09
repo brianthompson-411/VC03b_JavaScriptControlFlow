28b530efd20da4b902d032b232bc7ae41aac2a56
Brian Thompson <bthompson50@mail.valenciacollege.edu>
2021-11-08 17:55:55 -0800

    added the missing Part II of the assignment

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 let userChoice;
 let randomNumber;
 let computerChoice;
+let birthYear;
+let currentYear;
+let age;
+
+//Part I
 
 //userChoice = window.prompt("Heads or Tails?");
 userChoice = window.prompt("Please choose either: heads, or tails!");
@@ -25,3 +30,20 @@ if (userChoice == computerChoice){
 
 //Console confirmation behind the scenes
 console.log(computerChoice);
+
+
+//Part II
+birthYear = window.prompt("What year were you born in?");
+currentYear = new Date().getFullYear();
+age = currentYear - birthYear;
+
+if (age > 21){
+    window.alert("You are old enough to drink in the US");
+} else if(age =21){
+    window.alert("You are old enough to drink in the US...barley");
+} else{
+    window.alert("Sorry, you are not old enough to drink in the US");
+}
+
+
+
